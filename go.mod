736662924f05fad3bd1afd006b32ed84712874f2
fbuedding <100070891+fbuedding@users.noreply.github.com>
2023-11-09 18:30:53 +0100

    Added Types and ReadServiceGroups

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/go-chi/chi/v5 v5.0.10
 	github.com/google/uuid v1.4.0
 	github.com/gorilla/schema v1.2.0
+	github.com/niemeyer/golang v0.0.0-20110826170342-f8c0f811cb19
 )
--- pkg/iot-agent-sdk/iota-service.go
+++ pkg/iot-agent-sdk/iota-service.go
@@ -3,6 +3,7 @@ package iotagentsdk
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/niemeyer/golang/src/pkg/container/vector"
 	"io"
 	"net/http"
 )
@@ -11,26 +12,122 @@ const (
 	urlService = urlBase + "/iot/services"
 )
 
-type ReqCreateService struct {
-	Apikey     string `json:"apikey"`
-	Cbroker    string `json:"cbroker"`
-	EntityType string `json:"entity_type"`
-	Resource   string `json:"resource"`
+// see https://iotagent-node-lib.readthedocs.io/en/latest/api.html#service-group-datamodel
+type ServiceGroup struct {
+	Resource                     Resource          `json:"resource"`
+	Apikey                       Apikey            `json:"apikey"`
+	Timestamp                    *bool             `json:"timestamp,omitempty"`
+	Type                         string            `json:"type,omitempty"`
+	EntityType                   string            `json:"entity_type,omitempty"` //both type and EntityType work and are used both in documentation
+	Trust                        string            `json:"trust,omitempty"`
+	CbHost                       string            `json:"cbHost,omitempty"`
+	Lazy                         []LazyAttribute   `json:"lazy,omitempty"`
+	Commands                     []Command         `json:"commands,omitempty"`
+	Attributes                   []Attribute       `json:"attributes,omitempty"`
+	StaticAttributes             []StaticAttribute `json:"static_attributes,omitempty"`
+	InternalAttributes           []interface{}     `json:"internal_attributes,omitempty"`
+	ExplicitAttrs                string            `json:"explicitAttrs,omitempty"`
+	EntityNameExp                string            `json:"entityNameExp,omitempty"`
+	NgsiVersion                  string            `json:"ngsiVersion,omitempty"`
+	DefaultEntityNameConjunction string            `json:"defaultEntityNameConjunction,omitempty"`
+	Autoprovision                bool              `json:"autoprovision,omitempty"`
 }
 
-func (i IoTA) GetService() (*RespHealthcheck, error) {
+type Apikey string
+type Resource string
 
-	response, err := http.Get(fmt.Sprintf(urlHealthcheck, i.Host, i.Port))
-	if err != nil {
-		return nil, fmt.Errorf("Error while Healthcheck: %w", err)
+// these are all the same, but for typesafety differnt structs
+
+type Command struct {
+	Name     string              `json:"name"`
+	Type     string              `json:"type"`
+	Metadata map[string]Metadata `json:"metadata,omitempty"`
+}
+
+type Attribute struct {
+	Name     string              `json:"name"`
+	Type     string              `json:"type"`
+	Metadata map[string]Metadata `json:"metadata,omitempty"`
+}
+type LazyAttribute struct {
+	Name     string              `json:"name"`
+	Type     string              `json:"type"`
+	Metadata map[string]Metadata `json:"metadata,omitempty"`
+}
+type StaticAttribute struct {
+	Name     string              `json:"name"`
+	Type     string              `json:"type"`
+	Metadata map[string]Metadata `json:"metadata,omitempty"`
+}
+
+type Metadata struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+type MissingFields struct {
+	Fields  vector.StringVector
+	Message string
+}
+
+func (e *MissingFields) Error() string {
+	return fmt.Sprintf("Error %v: %s", e.Fields, e.Message)
+}
+
+func (sg ServiceGroup) Validate() error {
+
+	mF := &MissingFields{make(vector.StringVector, 0), "Missing fields"}
+
+	if sg.Resource == "" {
+		mF.Fields.Push("Resource")
+	}
+	if sg.Apikey == "" {
+		mF.Fields.Push("Apikey")
+	}
+	if sg.EntityType == "" && sg.Type == "" {
+		mF.Fields.Push("EntityType|Type")
 	}
 
-	responseData, err := io.ReadAll(response.Body)
+	if mF.Fields.Len() == 0 {
+		return nil
+	} else {
+		return mF
+	}
+}
+
+type RespReadServiceGroup struct {
+	Count    int            `json:"count"`
+	Services []ServiceGroup `json:"services"`
+}
+
+func (i IoTA) ReadServiceGroup(fs FiwareService, r Resource, a Apikey) (*RespReadServiceGroup, error) {
+	url := urlService + fmt.Sprintf("?r=%s&a=%s", r, a)
+
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, fmt.Sprintf(url, i.Host, i.Port), nil)
 
 	if err != nil {
-		return nil, fmt.Errorf("Error while Healthcheck: %w", err)
+		return nil, fmt.Errorf("Error while getting service: %w", err)
 	}
-	var respHealth RespHealthcheck
-	json.Unmarshal(responseData, &respHealth)
-	return &respHealth, nil
+	req.Header.Add("fiware-service", fs.Service)
+	req.Header.Add("fiware-servicepath", fs.ServicePath)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Error while getting service: %w", err)
+	}
+	defer res.Body.Close()
+
+	responseData, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Error while getting service: %w", err)
+	}
+
+	var respReadServiceGroup RespReadServiceGroup
+	json.Unmarshal(responseData, &respReadServiceGroup)
+	return &respReadServiceGroup, nil
+
 }
