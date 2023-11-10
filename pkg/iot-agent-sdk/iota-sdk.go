3227f5e75b021aa5b291df705e126bc2b520c37c
fbuedding <100070891+fbuedding@users.noreply.github.com>
2023-11-10 15:38:51 +0100

    cleaned types and added all the fields except device

--- pkg/iot-agent-sdk/iota-sdk.go
+++ pkg/iot-agent-sdk/iota-sdk.go
@@ -12,26 +12,6 @@ const (
 	urlHealthcheck = urlBase + "/iot/about"
 )
 
-type IoTA struct {
-	Host string
-	Port int
-}
-
-type FiwareService struct {
-	Service     string
-	ServicePath string
-}
-
-type RespHealthcheck struct {
-	LibVersion string `json:"libVersion"`
-	Port       string `json:"port"`
-	BaseRoot   string `json:"baseRoot"`
-	Version    string `json:"version"`
-}
-type ApiError struct {
-	Name    string `json:"name"`
-	Message string `json:"message"`
-}
 
 func (e ApiError) Error() string {
   return fmt.Sprintf("%s: %s", e.Name, e.Message)
