8f7fbd4b0653747b5ded454d6cde7db786f16128
fbuedding <100070891+fbuedding@users.noreply.github.com>
2024-03-29 19:05:20 +0100

    in memory session store now thread save

--- internal/pkg/sessionStore/inMemory.go
+++ internal/pkg/sessionStore/inMemory.go
@@ -2,36 +2,44 @@ package sessionStore
 
 import (
 	"errors"
+	"sync"
 
 	"github.com/google/uuid"
 )
 
-var Sessions = map[SessionToken]*Session{}
+var Sessions sync.Map
+var ErrUnkownType = errors.New("Unknown type")
 
 type InMemory struct{}
 
 func (im InMemory) Add(s *Session) (SessionToken, error) {
 	sessionToken := SessionToken(uuid.NewString())
-	Sessions[sessionToken] = s
+	Sessions.Store(sessionToken, s)
 	return sessionToken, nil
 }
 
 func (im InMemory) Get(st SessionToken) (*Session, error) {
-	session, exists := Sessions[st]
+	session, exists := Sessions.Load(st)
 	if !exists {
 		return nil, errors.New("Session not found!")
 	}
 
-	return session, nil
+	switch v := session.(type) {
+
+	case *Session:
+		return v, nil
+	default:
+		return nil, ErrUnkownType
+	}
 }
 
 func (im InMemory) Exists(st SessionToken) bool {
-	_, exists := Sessions[st]
+	_, exists := Sessions.Load(st)
 	return exists
 }
 
 func (im InMemory) Remove(st SessionToken) {
-	delete(Sessions, st)
+	Sessions.Delete(st)
 }
 
 func NewInMemory() *InMemory {
